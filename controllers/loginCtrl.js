8ab82d4fad75e6d117f85489b1ce7853c3f39ae3
amitrsingh1992 <singh.amit.10ee1074@gmail.com>
2017-01-12 19:21:40 +0530

    Latest

--- controllers/loginCtrl.js
+++ controllers/loginCtrl.js
@@ -1,11 +1,20 @@
-angular.module("dashboard").controller('loginCtrl',function ($scope,$auth,$state) {
+angular.module("dashboard").controller('loginCtrl',function ($scope,$auth,$state,$timeout) {
 console.log("loginCtrl...");
 $scope.loginFunction=function(){
+  $scope.loginProcess = true;
+  $scope.loginFailed = false;
   console.log($scope.data);
   $auth.login($scope.data,{"url":"http://192.168.0.171:3000/login"})
   .then(function () {
     $state.go("dashboard");
   }).catch(function (data) {
+    $scope.loginProcess = false;
+    $timeout(function () {
+      $scope.loginFailed = false;
+
+    },2000);
+
+    $scope.loginFailed = true;
     console.log(data);
   })
 }
--- controllers/signupCtrl.js
+++ controllers/signupCtrl.js
@@ -1,13 +1,17 @@
-angular.module("dashboard").controller('signupCtrl',function ($scope,$auth,$state) {
+angular.module("dashboard").controller('signupCtrl',function ($scope,$auth,$state,$timeout) {
 console.log("signupCtrl");
 $scope.signupFunction=function(){
+  $scope.signupProcess=true;
   console.log("signup....");
   $auth.signup($scope.data,{"url":"http://192.168.0.171:3000/signup"})
   .then(function () {
     $state.go("login");
   }).catch(function (data) {
     console.log(data);
-  })
+    $timeout(function () {
+      $scope.signupProcess = false;
+    },2000);
+    // $scope.signupProcess=false;
+});
 }
-
-})
+});
